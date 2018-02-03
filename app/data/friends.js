cc22b09bb2cd17fdc55ee4d4a71ec6ee0900de4a
Ayanna Scoon-Jerome <ayanna.scoon@gmail.com>
2018-02-03 03:38:14 -0500

    working public html pages

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,43 @@
-var friends = [];
+var friends = [
+{
+    "name" : "Issa",
+    "photo" : "https://media1.popsugar-assets.com/files/thumbor/4C3KJp0aFR0jXJw8CojZSaS4_Fc/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2018/01/30/817/n/1922153/e145f18eee1cac96_GettyImages-912017734/i/Issa-Rae-Black-Panther-LA-Premiere-2018.jpg",
+    "scores" : [5,1,4,2,3,1,4,5,1,2]
+},
+{
+    "name" : "Lupita",
+    "photo" : "http://i.dailymail.co.uk/i/pix/2018/01/30/20/48BD5A0C00000578-5331019-image-a-6_1517343721278.jpg",
+    "scores" : [2,3,4,4,5,2,1,3,1,5]
+},
+{
+    "name" : "Donald",
+    "photo" : "http://photos.laineygossip.com/articles/donald-glover-black-panther-30jan18-01.jpg";
+    "scores" : [3,2,2,4,5,5,4,2,4,1]
+},
+{
+    "name" : "Amber",
+    "photo" : "https://static.vibe.com/files/2018/01/amber-riley-black-panther-premiere-1517334469-373x560.jpg",
+    "scores" : [2,1,4,5,1,3,2,4,1,2]
+},
+{
+    "name" : "Angela",
+    "photo" : "https://thumbs.mic.com/YzBmMTRiZmIxNyMvMnlJZGJqeC1sNThGSkh1bWNOUWtJQmxSQmcwPS9maXQtaW4vNzYweDAvZmlsdGVyczpub191cHNjYWxlKCk6Zm9ybWF0KGpwZWcpOnF1YWxpdHkoODApL2h0dHBzOi8vczMuYW1hem9uYXdzLmNvbS9wb2xpY3ltaWMtaW1hZ2VzL3h3NWdkdWc3dmNxanRqZXozbmZsZHljNjlsbW9vN3JoZWppZTZ2ZWZlam1tdHp0cmprdnYya2l6a3ozb3JhbWwuanBn.jpg",
+    "scores" : [5,4,2,1,3,1,5,4,4,1]
+},
+{
+    "name" : "Janelle",
+    "photo" : "https://www.accessoriesmagazine.com/wp-content/uploads/2018/01/Black-Panther-premiere-Shutterstock-1-864x549.jpg",
+    "scores" : [5,4,2,1,3,1,5,4,4,1]
+},
+{
+    "name" : "Chadwick",
+    "photo" : "https://cdnph.upi.com/collection/fp/upi/11394/45c0dce616c6698de73e6c9fc6dc8add/Lupita-Nyongo-Chadwick-Boseman-attend-Black-Panther-premiere-in-LA_2_1.jpg",
+    "scores" : [5,4,2,1,3,1,5,4,4,1]
+},
+{
+    "name" : "Kashka",
+    "photo" : "";
+    "scores" : [5,4,2,1,3,1,5,4,4,1]
+},
+
+];
